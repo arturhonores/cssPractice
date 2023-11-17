32763899fc751ac46c42e5ab4f845f4c3b4e05c3
arturhonores <luis.arturhonores@gmail.com>
2023-11-17 23:53:17 +0100

    6to commit

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const style = document.documentElement.style
+const handleThemeDefault = () => {
+    style.setProperty("--primary", "#f77062");
+    style.setProperty("--secondary", "#1b998b");
+}
 const handleThemeBlue = () => {
-    style.setProperty("--primary", "blue");
-    style.setProperty("--secondary", "gray");
+    style.setProperty("--primary", "#190482");
+    style.setProperty("--secondary", "#7752FE");
+}
+
+
+//dropdown
+const themeButtonIcon = document.getElementById("themeButton-icon")
+const dropdownContainer = document.querySelector(".themeButtons__dropdown")
+const dropdownEffect = () => {
+    dropdownContainer.classList.toggle("visible")
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    const defaultButton = document.querySelector(".themeButtons__default")
     const blueButton = document.querySelector(".themeButtons__blue")
     blueButton.addEventListener("click", handleThemeBlue)
+    defaultButton.addEventListener("click", handleThemeDefault)
+    themeButtonIcon.addEventListener("click", dropdownEffect)
 })
